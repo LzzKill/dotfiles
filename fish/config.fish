3bca28029260da2ccbf2306b9e7609ceec7d7881
FZZkill <changzhijie11@outlook.com>
2022-11-05 18:06:25 +0800

    add .git

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 
     alias ls="ls -a";
-    alias bt transmission-cli;
     alias lg lazygit;
     alias neofetch "neofetch | lolcat";
     alias paccc "sudo pacman -Syu"

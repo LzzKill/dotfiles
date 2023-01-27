d7945a83f6bad5da3295f8a6312b4ad0510249df
FZZkill <changzhijie11@outlook.com>
2023-01-27 12:13:24 +0800

    a commit

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-    alias ls="ls -a";
+    alias ls="ls -a --color";
     alias lg lazygit;
     alias paccc "sudo pacman -Syu"
     # abbr

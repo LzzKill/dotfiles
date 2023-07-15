dbd4502446e55f64288541266c27bfb0e17d77a8
FZZkill <changzhijie11@outlook.com>
2023-07-15 13:53:42 +0800

    [fix] ls to exa

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-    alias ls="ls -a --color";
+    alias ls=exa;
     alias lg lazygit;
     alias paccc "sudo pacman -Syu"
     # abbr

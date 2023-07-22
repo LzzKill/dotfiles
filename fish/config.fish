0d8d22249c3798181161bf7d1492e7382a1a341f
FZZkill <changzhijie11@outlook.com>
2023-07-22 14:02:58 +0800

    [fix] alisa and fzf theme

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-    alias ls=exa;
-    alias lg lazygit;
-    alias paccc "sudo pacman -Syu"
+    abbr ls exa;
+    abbr lg lazygit;
+    abbr paccc "sudo pacman -Syu"
     # abbr
 end
 

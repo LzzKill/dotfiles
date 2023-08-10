c6fff24eebdcb2282cb8657b76309c1ec1d82891
FZZkill <changzhijie11@outlook.com>
2023-08-10 20:40:38 +0800

    [add] abbr ll "exa -l"

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 
     abbr ls exa;
+    abbr ll "exa -l"
     abbr lg lazygit;
     abbr paccc "sudo pacman -Syu"
     # abbr
